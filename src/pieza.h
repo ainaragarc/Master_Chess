31ee92f807b3567b1f9d6d0332ce2ab061c07c37
daniel-fuertes <80775475+daniel-fuertes@users.noreply.github.com>
2025-04-30 00:35:48 +0200

    cambio en piezas, clase posicion y funcion mueve

--- src/pieza.h
+++ src/pieza.h
@@ -1,13 +1,24 @@
 #pragma once
 #include "color.h"
+#include "Posicion.h"
+
+//Enum class para los tipos de pieza, posible uso con switch case en mundo/tablero para dibujar, mover
+enum class Tipo { PEON, CABALLO, TORRE, ALFIL, REY, REINA };
 
 class Pieza {
 	float Tamaño;
-	char tipo;
+	int cantidad;
+	
 	Color color;
+	Posicion posicion;
+	Tipo tipo;
+	
 
 public:
 	void dibuja();
-	void mueve();
+
+	//propuesta de devolver una posición al mover una ficha, esta hará una cosa u otra dependiendo de qué tipo es.l
+	Posicion mueve(Pieza& ficha);
 	void inicializa();
+	
 };
---
+++ src/posicion.h
@@ -0,0 +1,6 @@
+#pragma once
+class Posicion
+{
+	int fila, columna;
+};
+

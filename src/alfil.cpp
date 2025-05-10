9565fba5bfc1fafc204cd7e237bff9b98c9fae15
ainaragarc <ainara.garcia@alumnos.upm.es>
2025-05-10 18:09:06 +0200

    errores que no se habian subido bien
    :)

--- src/alfil.cpp
+++ src/alfil.cpp
@@ -6,20 +6,22 @@
 
 vector<Posicion> Alfil::posiciones_posibles() {
 	vector<Posicion> posibles;	
-	// quiero que recorra las diagonales
+	// quiero que recorra las diagonales desde donde este, hasta que toque final de tablero o una pieza
 
 	Posicion direccion[4] = { {1,1},{1,-1},{-1,-1},{-1,1} };
-
+	
 	for (auto a :direccion) {
-		
+		Posicion pos = get_posicion();
+		//ESTA POSICION SE IRA MOVIENDO A LO LARGO DE LA DIAGONAL, Y RESETEARA PARA LA SIGUIENTE
 		for (int i = 1; i < Tablero::get_numCas(); i++) {
-			posicion.Fila += a.Fila;
-			posicion.Columna += a.Columna;
-			if (posicion.Fila < 0 || posicion.Fila >= Tablero::get_numCas() || posicion.Columna < 0 || posicion.Columna >= Tablero::get_numCas()) {
+			pos.Fila += a.Fila;
+			pos.Columna += a.Columna;
+			if (pos.Fila < 0 || pos.Fila >= Tablero::get_numCas() || pos.Columna < 0 || pos.Columna >= Tablero::get_numCas()) {
 				break; // Salir si se sale del tablero
 			}
 			posibles.push_back(pos);
 			if (Tablero::hay_pieza(pos)) { break; }// Salir si hay una pieza en esa posicion PERO DESPUES DE GUARDADRLA
+			//COMO SE PUEDE COMER A TODAS LAS PIEZAS PODRA IR A TODAS LAS POSCIONES, SALVO SI ES UN REY!!!
 		}
 	}
 

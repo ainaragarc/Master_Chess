3a95547230a0d2ae80e2a409bb8a6bf7b10e2a4f
ainaragarc <ainara.garcia@alumnos.upm.es>
2025-05-10 15:24:56 +0200

    un pequeño traspies

--- src/alfil.h
+++ src/alfil.h
@@ -5,11 +5,14 @@ class Alfil : public Pieza{
 	void set_tipo() { tipo = Tipo::ALFIL; }
 	void set_cantidad() { cantidad = 1; }
 
+
 public:
 	vector<Posicion> posiciones_posibles() override;
+	void set_posicion_ini() { posicion.Fila = 0; posicion.Columna = 0; };
+
+
 
 	Alfil(Posicion pos) {posicion = pos;}
 	Alfil() = default;
-};
-	void set_posicion_ini() { posicion.Fila = 0; posicion.Columna = 0; };
+
 };

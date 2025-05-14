01ff99dce9dbf7f3d8d373f41c678eb4d5f687d3
maarioo13 <m.hgonzalez@alumnos.upm.es>
2025-05-15 01:33:49 +0200

    clase GestorEstados
    prototipo de clase para la gestión de los estados del juego. en principio se incluirá también aquí la gestión de los turnos

--- src/GestorEstados.h
+++ src/GestorEstados.h
@@ -1,1 +1,20 @@
 #pragma once
+#include "Mundo.h"
+
+class GestorEstados {
+
+private:
+    enum Estado { MENU, JUGANDO, PAUSA, FIN }estado_actual{};
+    //Mundo mundo; descomentar cuando este terminada la implementación
+
+public:
+    
+    void cambiar_estado(Estado nuevo) { estado_actual = nuevo; }//funcion para cambiar de estado
+    Estado get_estado() const { return estado_actual; }
+
+    void inicializa();
+    void dibuja();
+    void mueve();
+    void tecla(unsigned char key);
+    void raton(int button, int state, int x, int y);
+};

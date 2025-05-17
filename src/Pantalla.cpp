18636a076e0211d1e71ee49837e8a1ca562158be
maarioo13 <m.hgonzalez@alumnos.upm.es>
2025-05-17 23:56:10 +0200

    pantalla.h
    no se subía porque no había guardado

---
+++ src/Pantalla.cpp
@@ -0,0 +1,16 @@
+#include "Pantalla.h"
+
+void Pantalla::dibuja() {
+
+}
+void Pantalla::actualiza(){
+
+}
+
+void Pantalla::tecla(unsigned char key) {
+
+}
+
+void Pantalla::raton(int button, int state, int x, int y) {
+
+}
--- src/Pantalla.h
+++ src/Pantalla.h
@@ -1,1 +1,10 @@
 #pragma once
+
+class Pantalla {
+public:
+    virtual void dibuja();//funcion para dibujar pantallas
+    virtual void actualiza();//funcion para actualizar pantallas
+    virtual void tecla(unsigned char key);//funcin para usar teclas en las pantallas
+    virtual void raton(int button, int state, int x, int y);//funcion para usar el raton en las pantallas
+    virtual ~Pantalla() {}
+};

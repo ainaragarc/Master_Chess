cca987e4332364ca095d37172b17edcb2480e83c
Ralul55 <130755244+Ralul55@users.noreply.github.com>
2025-05-29 15:55:23 +0200

    Update brocha.cpp

--- src/brocha.cpp
+++ src/brocha.cpp
@@ -6,29 +6,7 @@ void Brocha::dibuja_t(int numCasillas, float TamCuadrado) {
 
     BrochaPantallas::configurar_proyeccion_pantalla(4.0);
 
-    float BOARD_OFFSET = -((numCasillas * TamCuadrado) / 2.0f);
-
-    bool white = true;
-
-    for (int i = 0; i < numCasillas; i++) {
-        for (int j = 0; j < numCasillas; j++) {
-            if (white)
-                glColor3f(1.0f, 1.0f, 1.0f); // Blanco
-            else
-                glColor3f(0.0f, 0.0f, 0.0f); // Negro
-
-            glBegin(GL_QUADS);
-            glVertex2f(j * TamCuadrado + BOARD_OFFSET, i * TamCuadrado + BOARD_OFFSET);
-            glVertex2f((j + 1) * TamCuadrado + BOARD_OFFSET, i * TamCuadrado + BOARD_OFFSET);
-            glVertex2f((j + 1) * TamCuadrado + BOARD_OFFSET, (i + 1) * TamCuadrado + BOARD_OFFSET);
-            glVertex2f(j * TamCuadrado + BOARD_OFFSET, (i + 1) * TamCuadrado + BOARD_OFFSET);
-            glEnd();
-
-            white = !white;
-        }
-        if (numCasillas % 2 == 0)
-            white = !white;
-    }
+    BrochaPantallas::insertarimagen("imagenes/FondoTablero.png", 4.0, 4.0, 4.0, 4.0);
 
 }
 
